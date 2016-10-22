0c6251dc007104cd35b6211912ca5dc8fdae01d4
Rishabh Manocha <rmanoch@elance-odesk.com>
2016-10-22 13:49:21 -0700

    actually adding some stuff

--- pics_queue.py
+++ pics_queue.py
@@ -1,9 +1,23 @@
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+
 import records
 
 from time import sleep
 
+S3_BUCKET = "pi-photos-test"
+
+def log_info(statement):
+    print statement
+
 def send_to_s3(filename):
-    pass
+    log_info("Uploading %s" % filename)
+    conn = S3Connection()
+    bucket = conn.get_bucket(S3_BUCKET)
+    #key = Key(bucket)
+    key = bucket.get_key(filename, validate=False)
+    key.set_contents_from_filename(filename)
+    log_info("done")
 
 def mark_uploaded(db, row_id):
     pass
@@ -12,10 +26,15 @@ if __name__=="__main__":
     db = records.Database("sqlite:///rpi_camera.db")
 
     while True:
-        rows = db.query("select * from rpi_camera where uploaded is false order by created_date")
+        rows = db.query("select * from rpi_camera where uploaded = 0")
+        import ipdb; ipdb.set_trace()
         if rows:
+            log_info("found %d not uploaded files" % len(rows))
+
             filename = rows[0]["filename"]
             row_id = rows[0]["id"]
+
+            log_info("Uploading %s with id %d" % (filename, row_id))
 
             send_to_s3(filename)
             mark_uploaded(db, row_id)
