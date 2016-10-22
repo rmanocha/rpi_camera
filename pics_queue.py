97730c1389340bf38abdbeac79ff0d09cee91f6f
Rishabh Manocha <rmanoch@elance-odesk.com>
2016-10-22 14:15:09 -0700

    some cleanup

--- pics_queue.py
+++ pics_queue.py
@@ -7,27 +7,35 @@ from time import sleep
 
 S3_BUCKET = "pi-photos-test"
 
+SELECT_STATEMENT = "select * from rpi_camera where uploaded = 0"
+
+MARK_UPLOADED_STATEMENT = "update rpi_camera set uploaded = 1 where row_id = %s"
+
 def log_info(statement):
     print statement
 
 def send_to_s3(filename):
     log_info("Uploading %s" % filename)
+
     conn = S3Connection()
+
     bucket = conn.get_bucket(S3_BUCKET)
-    #key = Key(bucket)
+
     key = bucket.get_key(filename, validate=False)
     key.set_contents_from_filename(filename)
+
     log_info("done")
 
 def mark_uploaded(db, row_id):
-    pass
+    db.query(MARK_UPLOADED_STATEMENT % row_id)
+
+    log_info("Marked row %d as uploaded" % row_id)
 
 if __name__=="__main__":
     db = records.Database("sqlite:///rpi_camera.db")
 
     while True:
         rows = db.query("select * from rpi_camera where uploaded = 0")
-        import ipdb; ipdb.set_trace()
         if rows:
             log_info("found %d not uploaded files" % len(rows))
 
